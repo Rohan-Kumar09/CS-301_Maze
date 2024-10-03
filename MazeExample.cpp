9ad8e2604194498c64e39838448977ddd58f1ad5
Rohan KumarPC <rkumar51@horizon.csueastbay.edu>
2024-10-02 17:50:57 -0700

    Solved

--- MazeExample.cpp
+++ MazeExample.cpp
@@ -23,12 +23,39 @@ using namespace std;
 
 
 void SolveMaze(int row, int col, char maze[][100], int rowSz, int colSz, bool& foundEnd, string& finaldir, string currDir) {
+	if (foundEnd){ // if found the end
+		return;
+	}
+	if (row < 0 || col < 0 || row > rowSz || col > colSz){
+		return; // if out of bounds
+	}
+	if (maze[row][col] == 'F'){
+		foundEnd = true;
+		finaldir = currDir; // found the end, the current set of directions is the final set of directions
+		return; // maze is finished
+	}
+	if (maze[row][col] == '.'){ // if already visited
+		return; // bread crums
+	}
+	if (maze[row][col] == '@'){
+		return; // walls
+	}
 
+	maze[row][col] = '.'; // leave bread crums
+
+	SolveMaze(row - 1, col, maze, rowSz, colSz, foundEnd, finaldir, currDir+"U"); // Up
+	SolveMaze(row + 1, col, maze, rowSz, colSz, foundEnd, finaldir, currDir+"D"); // Down
+	SolveMaze(row, col - 1, maze, rowSz, colSz, foundEnd, finaldir, currDir+"L"); // Left
+	SolveMaze(row, col + 1, maze, rowSz, colSz, foundEnd, finaldir, currDir+"R"); // Right
+
+	if (!foundEnd) {
+		maze[row][col] = ' ';
+	}
 }
 
 int main()
 {
-	ifstream mazeFile("maze.txt");
+	ifstream mazeFile("C:/Users/rohan/OneDrive/Desktop/CS-301_Maze/maze.txt"); // absolute path is needed for debugging.
 	char maze[100][100]; // make larger than required
 
 	// Initialize the maze
